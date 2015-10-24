abf8432d19238d5a06101e9656d9182a75d99bf4
Jared Jolton <jared.jolton@gmail.com>
2015-10-23 22:35:44 -0600

    broken?

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var random = 0;
+var song;
+
+function preload() {  
+  song = loadSound('assets/newb.mp3');
+}
+
+function setup(){
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(song);
+}
+
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function mousePressed() {
+  if ( song.isPlaying() ) { 
+    song.stop();
+  } else {
+    song.play();
+  }
+}
+
+function draw(){
+ random = getRandom(0,250);
+ background(30);
+ ambientLight(100);
+ pointLight(250, 250, 250, 100, 100, 0);
+ specularMaterial(56);
+ //sphere(200, 128);
+ var vol = analyzer.getLevel();
+ stroke(0);
+ sphere(width/2, height/2, 10+vol*200, 10+vol*200);
+}
